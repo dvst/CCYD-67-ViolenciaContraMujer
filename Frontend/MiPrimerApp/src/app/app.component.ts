c87a3d25bca43f256f8980dbd1cd17f1049cbf54
Javier E. Perez P <javier.perez.puello@gmail.com>
2018-10-20 23:46:25 -0500

    eliminando referencias inecesarias

--- Frontend/MiPrimerApp/src/app/app.component.ts
+++ Frontend/MiPrimerApp/src/app/app.component.ts
@@ -2,17 +2,10 @@ import { Component, ViewChild } from '@angular/core';
 
 import { Platform, MenuController, Nav } from 'ionic-angular';
 
-import { ListPage } from '../pages/list/list';
-
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { LoginPage } from '../pages/login/login';
-import { ElementosPage } from '../pages/elementos/elementos';
-import { TabsPage } from '../pages/tabs/tabs';
-import { SliderPage } from '../pages/slider/slider';
 import { BienvenidaPage } from '../pages/bienvenida/bienvenida';
 import { PerfilPage } from '../pages/perfil/perfil';
-import { PerfilCrearPage } from '../pages/perfil-crear/perfil-crear';
 import { HistoriasPage } from '../pages/historias/historias';
 import { AcercaPage } from '../pages/acerca/acerca';
 
@@ -38,14 +31,8 @@ export class MyApp {
     this.pages = [
       { title: 'Bienvenida', component: BienvenidaPage },
       { title: 'Seleccionar perfil', component: PerfilPage },
-      { title: 'My First List', component: ListPage },
-      { title: 'Login', component: LoginPage },
-      { title: 'Elementos', component: ElementosPage },
-      { title: 'Tabs', component: TabsPage },
-      { title: 'Slider', component: SliderPage },
-      { title: 'Bienvenida', component: BienvenidaPage },
+      { title: 'Historias', component: HistoriasPage },
       { title: 'Acerca de la app', component: AcercaPage },
-
     ];
   }
 
