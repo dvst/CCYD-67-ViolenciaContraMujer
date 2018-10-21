00de5a445b24069ba6d53dc91bcc29695a8e7e51
Javier E. Perez P <javier.perez.puello@gmail.com>
2018-10-21 00:44:10 -0500

    agregando pagina: "trofeo" y eliminando paginas innecesarias

--- Frontend/MiPrimerApp/src/pages/trofeo/trofeo.ts
+++ Frontend/MiPrimerApp/src/pages/trofeo/trofeo.ts
@@ -1,32 +1,24 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import { LoginPage } from '../login/login';
-import { ListPage } from '../list/list';
 
 /**
- * Generated class for the TabsPage page.
+ * Generated class for the TrofeoPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
 
 @Component({
-  selector: 'page-tabs',
-  templateUrl: 'tabs.html',
+  selector: 'page-trofeo',
+  templateUrl: 'trofeo.html',
 })
-export class TabsPage {
+export class TrofeoPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.tab1 = LoginPage;
-    this.tab2 = ListPage;
-
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad TabsPage');
+    console.log('ionViewDidLoad TrofeoPage');
   }
-
-  tab1;
-  tab2;
 
 }
