1d8b1cac2fd6391ee1e6b7467079b51516f62d42
Javier E. Perez P <javier.perez.puello@gmail.com>
2018-10-21 00:16:24 -0500

    agregado mensaje de bienvenida a usuario seleccionado de perfil

--- Frontend/MiPrimerApp/src/pages/perfil/perfil.ts
+++ Frontend/MiPrimerApp/src/pages/perfil/perfil.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { HistoriasPage } from '../historias/historias';
+import { ToastController } from 'ionic-angular';
+
 
 /**
  * Generated class for the PerfilPage page.
@@ -14,11 +17,30 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class PerfilPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public toastCtrl: ToastController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PerfilPage');
   }
+
+  irPagina(type,nombre){
+    let componente:any; 
+    switch(type){
+      case "historias":
+        componente=HistoriasPage;
+        break;
+    }
+
+    const toast = this.toastCtrl.create({
+      message: 'Bienvenido de vuelta ' + nombre,
+      duration: 3000,
+      position: 'top'
+    });
+    toast.present();
+
+    this.navCtrl.setRoot(componente)
+  }
+
 
 }
