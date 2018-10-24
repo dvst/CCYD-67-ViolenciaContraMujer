1d4396780740f72bac56eaf3fc13f0a2fe99acd3
Javier E. Perez P <templeofdvst@gmail.com>
2018-10-24 12:22:36 -0500

    creando scaffold de modelo profile
    rails g scaffold profile name:string gender:string age:integer location:string

---
+++ BackEnd/db/migrate/20181024171639_create_profiles.rb
@@ -0,0 +1,12 @@
+class CreateProfiles < ActiveRecord::Migration[5.2]
+  def change
+    create_table :profiles do |t|
+      t.string :name
+      t.string :gender
+      t.integer :age
+      t.string :location
+
+      t.timestamps
+    end
+  end
+end
